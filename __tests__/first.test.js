e844a6a26a71e2d33e8da4e2bd1f359d16f291ff
Kishan Tiwari <kishanmanitiwari@gmail.com>
2026-08-02 13:30:44 +0530

    Tests and CI_CD

---
+++ __tests__/first.test.js
@@ -0,0 +1,4 @@
+// First Test
+test("2 + 2 = 4", () => {
+    expect(2 + 2).toBe(4);
+});
--- index.js
+++ index.js
@@ -53,3 +53,5 @@ app.use((err, req, res, next) => {
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
+
+export default app;
