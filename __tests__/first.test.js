bf89085de9330e344e38f5740a84f23c21d156b7
Kishan Tiwari <kishanmanitiwari@gmail.com>
2026-08-03 11:45:22 +0530

    Added CD Job

--- __tests__/first.test.js
+++ __tests__/first.test.js
@@ -6,6 +6,8 @@ test("6-2 = 4", () => {
   expect(6 - 2).toBe(4);
 });
 
+
+// Purposefully wrtiing wring test case to check the CI/CD pipeline for failure
 test("6-2 = 4", () => {
   expect(3 - 2).toBe(4);
 });
