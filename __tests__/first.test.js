8bd80f0a337cd9880ce692245336abd555db879f
Kishan Tiwari <kishanmanitiwari@gmail.com>
2026-08-03 11:46:10 +0530

    Fxied Test Case which was failing

--- __tests__/first.test.js
+++ __tests__/first.test.js
@@ -6,8 +6,7 @@ test("6-2 = 4", () => {
   expect(6 - 2).toBe(4);
 });
 
-
 // Purposefully wrtiing wring test case to check the CI/CD pipeline for failure
 test("6-2 = 4", () => {
-  expect(3 - 2).toBe(4);
+  expect(3 - 2).toBe(1);
 });
